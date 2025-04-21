29afd72b77424564421823f4e9353281ab7876af
Arif Mohammed <Arif.Mohammed@nexgeninc.com>
2025-04-20 19:43:28 -0500

    renamed GetAllBooks to GetBookById

--- node-api-with-cosmos-db/src/functions/GetBookById.js
+++ node-api-with-cosmos-db/src/functions/GetBookById.js
@@ -1,27 +1,26 @@
 const { app, input, output } = require('@azure/functions');
 
 // Define Cosmos DB input binding correctly
-const cosmosInput = input.cosmosDB({
+const docById = input.cosmosDB({
     databaseName: 'BookDB',
     containerName: 'BookContainer',
     id: '{Query.id}',                           // Using query parameter for 'id'
     partitionKey: '{Query.partitionKeyValue}',  // Using query parameter for partitionKey
     connection: 'COSMOS_DB_CONNECTION_STRING',
 });
-  
-app.http('GetAllBooks', {
+
+app.http('GetBookById', {
     methods: ['GET'],
     authLevel: 'anonymous',
     // Pass input binding correctly as an object
     /*inputs: {
       book: cosmosInput,  // 'book' is now correctly defined as a key with the input binding
     },*/
-    extraInputs: [cosmosInput],
+    extraInputs: [docById],
     handler: async (request, context) => {
-        const book = context.extraInputs.get(cosmosInput);  
+        const book = context.extraInputs.get(docById);  
         //context.log(`Received book object: ${JSON.stringify(book)}`);
         //process.env.AZURE_COSMOS_VERBOSE_LOGGING = 'true';
-
         if (!book) {
             return {
                 status: 404,
@@ -30,7 +29,8 @@ app.http('GetAllBooks', {
         } else {
             return {
                 status: 200,
-                jsonBody: book
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(book, null, 2)
             };
         }        
     }
